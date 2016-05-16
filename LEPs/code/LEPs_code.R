9018baa0f25cc00ea0127d11d6b840237e07ed3c
Konstantinos Daras <konstantinos.daras@gmail.com>
2016-05-16 17:01:43 +0100

    Load Census CSV files in postgis

--- LEPs/code/LEPs_code.R
+++ LEPs/code/LEPs_code.R
@@ -1,11 +1,12 @@
-source("source/postgis_functions.r")
+source("/media/kd/Data/Dropbox/Repos/r_source/postgis_functions.r")
 library(rgeos)
 library(rgdal)
 library(dplyr)
 
-# setwd("/media/kd/Data/Dropbox/Repos/rds/")
-setwd("D:/Dropbox/Repos/rds/")
+setwd("/media/kd/Data/Dropbox/Repos/rds/")
+# setwd("D:/Dropbox/Repos/rds/")
 
+pwd <- readline(prompt="Enter Postgres password: ")
 # Read LEPs lookup table
 leps_lut <- read.csv(paste(getwd(),"/LEPs/leps.csv", sep = ""), sep="|")
 leps_ids <- read.csv(paste(getwd(),"/LEPs/lepids.csv", sep = ""), sep="|", colClasses = c("character", "character"))
@@ -13,8 +14,9 @@ leps_lut$lepid <- leps_ids[match(leps_lut$lep, leps_ids$lepname),1]
 write.table(leps_lut, file = paste(getwd(),"/LEPs/leps.csv", sep = ""), row.names=FALSE, quote = FALSE, na="", sep="|")
 
 # Add OAs and LADs in PostGIS
-db <- create_db("cdrcdb", "postgres", "kd")
-con <- odbcConnect("cdrcdb", uid = "postgres", pwd="kd")
+db <- create_db("cdrcdb", "kd", pwd, spacetable= "open_path", extensions = c("POSTGIS"))
+# test <- create_db("prescdb", "kd", pwd, spacetable= "safe_path", extensions = c("POSTGIS","POSTGIS_topology"))
+con <- odbcConnect("cdrcdb", uid = "kd", pwd= pwd)
 username <- unlist(strsplit(unlist(strsplit(attributes(con)$connection.string, ";"))[5], "="))[2]
 db_name <- unlist(strsplit(unlist(strsplit(attributes(con)$connection.string, ";"))[2], "="))[2]
  
@@ -34,7 +36,7 @@ odbcQuery(con, "DROP TABLE IF EXISTS leps_fin;")
 odbcQuery(con, "CREATE TABLE leps_fin AS SELECT ST_UnaryUnion(ST_Collect(geom)), lad_lep.lepid, lad_lep.lep FROM lad_lep  GROUP BY lad_lep.lep, lad_lep.lepid;")
 
 # Export LEPs shapefile
- system(paste0('pgsql2shp -f ', getwd(), '/LEPs/data/leps.shp -h localhost -u ', username, ' -P kd ', db_name,' "SELECT * FROM leps_fin"'))
+system(paste0('pgsql2shp -f ', getwd(), '/LEPs/data/leps.shp -h localhost -u ', username, ' -P ', pwd, ' ', db_name,' "SELECT * FROM leps_fin"'))
  
 
 # Prepare the lookup tables
@@ -67,17 +69,24 @@ for (i in 1:nrow(pg_tables[1])){
   # Create MSOA tables
   odbcQuery(con, paste0("DROP TABLE IF EXISTS ",pg_tables$pg_tables[i],"_msoa;"))
   odbcQuery(con, paste0("CREATE TABLE ",pg_tables$pg_tables[i],"_msoa (GeographyCode varchar, ", var_lst, ");"))
-  
 }
 
 # Load Census CDRC Data Pack CSVs in PostGIS
 for (i in 1:nrow(lookup_lad[2])){
-  for (i in 1:nrow(pg_tables[1])){
-   
-     
+  
+  dir <- as.character(lookup_lad$LAD11NM[i])
+  print(paste("Load the CSV files of",dir))
+  
+  for (x in 1:nrow(pg_tables[1])){
+    tb <- as.character(tolower(pg_tables$pg_tables[x])) 
+    odbcQuery(con, paste0("COPY ",tb,"_oa FROM '", getwd(),"/s_data/england_wales_zip/", dir, "/tables/", 
+                           as.character(pg_tables$pg_tables[x]),"_oa11.csv' DELIMITER ',' CSV HEADER;"))
+    odbcQuery(con, paste0("COPY ",tb,"_lsoa FROM '", getwd(),"/s_data/england_wales_zip/", dir, "/tables/", 
+                          as.character(pg_tables$pg_tables[x]),"_lsoa11.csv' DELIMITER ',' CSV HEADER;"))
+    odbcQuery(con, paste0("COPY ",tb,"_msoa FROM '", getwd(),"/s_data/england_wales_zip/", dir, "/tables/", 
+                          as.character(pg_tables$pg_tables[x]),"_msoa11.csv' DELIMITER ',' CSV HEADER;"))
   }
 }
-
 
 
 
